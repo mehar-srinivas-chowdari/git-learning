d94d2c5d68b2a0750034ee16ffd34c8033fff445
mehar-srinivas-chowdari <84114918+mehar-srinivas-chowdari@users.noreply.github.com>
2022-10-19 12:16:26 +0530

    Update connection_check.py

--- connection_check.py
+++ connection_check.py
@@ -1,7 +1,12 @@
+'''
 import psycopg2
 
 conn = psycopg2.connect(host='localhost',port=5432,database='postgres',user='postgres',password=1)
+'''
 
 #returns zero if the connection is active or returns a non zero value if the connection is closed or broken
 
-print(conn.closed)
+def check_connection(conn):
+    return conn.closed
+
+print(check_connection(conn))

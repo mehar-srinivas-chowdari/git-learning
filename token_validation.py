4867d4bf6a7ccc5ff6d17a8443a2c926d08a5b01
mehar-srinivas-chowdari <84114918+mehar-srinivas-chowdari@users.noreply.github.com>
2022-10-19 12:16:55 +0530

    Update token_validation.py

--- token_validation.py
+++ token_validation.py
@@ -1,4 +1,7 @@
+'''
 import boto3
+import time
+import datetime
 
 client = boto3.client('rds')
 
@@ -8,26 +11,24 @@ user = 'sample_user'
 region = 'us-east-1'
 
 token = client.generate_db_auth_token(DBHostname = hostname , Port = port , DBUsername = user, Region = region)
-
+'''
 # retriving by parsing date_time and expiry_time from the token
 
-ind = token.index('X-Amz-Date=')
-date_time_str = token[ind+11 : ind+26]
+def token_validation(token):
+    ind = token.index('X-Amz-Date=')
+    date_time_str = token[ind+11 : ind+26]
 
-exp = token.index('X-Amz-Expires=')
-expiry_val = int(token[exp+14 : exp+14+token[exp+14:].index('&')])
+    exp = token.index('X-Amz-Expires=')
+    expiry_val = int(token[exp+14 : exp+14+token[exp+14:].index('&')])
 
-import time
-import datetime
+    ist = 19800  # neglect if using GMT
+    ust = time.mktime(datetime.datetime.strptime(date_time_str,"%Y%m%dT%H%M%S").timetuple())
+    creation_time = ust + ist
 
-ist = 19800
+    expiry_time = creation_time + expiry_val
 
-ust = time.mktime(datetime.datetime.strptime(date_time_str,"%Y%m%dT%H%M%S").timetuple())
+    current_time = int(datetime.datetime.now().timestamp())
 
-creation_time = ust + ist
+    return current_time < expiry_time  # true if token is not expired
 
-expiry_time = creation_time + expiry_val
-
-current_time = int(datetime.datetime.now().timestamp())
-  
-print(current_time < expiry_time)  # true if token is not expired
+print(token_validation(token))
